b25819666e2a4f97d4ddec1f1e49d270f05b548f
drodriguezg28 <drodriguezg28@iesalbarregas.es>
2025-12-14 17:16:58 +0100

    Add files via upload

---
+++ AprovDBbal.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+# Instalación de nginx
+sudo apt update
+echo "Repositorios actualizados."
+sudo apt install nginx haproxy -y
+echo "Nginx instalado correctamente."
+
+# Configuración del servidor HAProxy
+cd /etc/haproxy/
+sudo cp haproxy.cfg haproxy.cfg.bak
+cat <<EOF > haproxy.cfg
+
+global
+    log /dev/log local0
+    maxconn 2000
+    user haproxy
+    group haproxy
+    daemon
+
+defaults
+    log     global
+    mode    tcp                    
+    option  tcplog                 
+    timeout connect 5000ms
+    timeout client  50000ms
+    timeout server  50000ms
+
+frontend db_front
+    bind *:3306
+    mode tcp
+    default_backend balancea_db
+
+backend balancea_db
+    mode tcp
+    balance roundrobin
+    option tcp-check
+    server db1 192.168.30.10:3306 check
+    server db2 192.168.30.11:3306 check
+
+EOF
+
+# Habilitar y reiniciar el servicio HAProxy
+sudo systemctl enable haproxy
+sudo systemctl restart haproxy
+
+
+#sudo route del default
